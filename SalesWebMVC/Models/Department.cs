acbf307b3b5d07475495dfdb278e2860800cc947
dannsrkk <danieconomics@hotmail.com>
2024-04-05 21:44:34 -0300

    Other Entities and Second Migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
-
 namespace SalesWebMVC.Models
 {
 	public class Department
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+
+		public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+		public Department()
+		{
+		}
+		public Department(int id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+
+		public void AddSellers(Seller sl)
+		{
+			Sellers.Add(sl);
+		}
+
+		public void RemoveSellers(Seller sl)
+		{
+			Sellers.Remove(sl);
+		}
+
+		public double TotalSales(DateTime initial, DateTime final)
+		{
+			return Sellers.Sum(sl => sl.TotalSales(initial, final));
+		}
 	}
 }
---
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMVC.Models.Enums
+{
+	public enum SaleStatus
+	{
+		Pending = 1,
+		Billed = 2,
+		Canceled = 3
+	}
+}
