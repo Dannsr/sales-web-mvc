63fc406f0e754cc48695a4070625cb163b655741
dannsrkk <danieconomics@hotmail.com>
2024-04-07 21:06:43 -0300

    Seller Details and Eager Loading

--- SalesWebMVC/Service/SellerService.cs
+++ SalesWebMVC/Service/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Service
 {
@@ -27,7 +28,7 @@ namespace SalesWebMVC.Service
 
 		public Seller FindById(int id)
 		{
-			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+			return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
 		}
 
 		public void Remove(int id)
